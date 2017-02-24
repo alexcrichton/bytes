490de63610673b2d79abed3a9ac414e2392b4061
Travis CI User <travis@example.org>
2017-02-24 18:38:55 +0000

    Documentation for carllerche/bytes@9eb5bd5

---
+++ implementors/core/marker/trait.Copy.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["byteorder"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='enum' href='byteorder/enum.BigEndian.html' title='byteorder::BigEndian'>BigEndian</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='enum' href='byteorder/enum.LittleEndian.html' title='byteorder::LittleEndian'>LittleEndian</a>",];implementors["bytes"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='enum' href='bytes/enum.BigEndian.html' title='bytes::BigEndian'>BigEndian</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='enum' href='bytes/enum.LittleEndian.html' title='bytes::LittleEndian'>LittleEndian</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
